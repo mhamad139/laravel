de61cabb5d14fff7e92501fa8a8010da7323215e
mhamad othman <mhamadothman093@gmail.com>
2022-04-05 17:35:45 +0300

    init

---
+++ resources/views/Profile.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style type="text/css">
+    	body{
+    		margin: 0px;
+    		padding: 0px;
+    		background: #f1f1f1;
+    		font-family: arial;
+    		box-sizing: border-box;
+    	}
+    	.card-container{
+    		width: 300px;
+    		height: 500px;
+    		background: #FFF;
+    		border-radius: 6px;
+    		position: absolute;
+    		top: 50%;
+    		left: 50%;
+    		transform: translate(-50%,-50%);
+    		box-shadow: 0px 1px 10px 1px #000;
+    	}
+    	.card-top{
+    		height: 150px;
+    		background: #7f00ff;
+    	}
+    	.card-bottom{
+    		height: 280px;
+    		background: #fff;
+    		padding: 20px;
+    		padding-top: 40px;
+            color: #7f00ff;
+    	}
+    	.image{
+    		background: white;
+    		width: 80px;
+    		height: 80px;
+    		border-radius: 50%;
+    		transform: translate(100px,100px);
+    		padding: 5px;
+    	}
+    	.image img{
+    		width: 80px;
+    		height: 80px;
+    		border-radius: 50%;
+    	}
+    </style>
+    <title>Profile</title>
+</head>
+<body>
+    <div class="card-container">
+    	<div class="card-top">
+    		<div class="image">
+    		<img src="image\photo.jpg">
+    		</div>
+    	</div>
+    	<div class="card-bottom">
+    		<div>
+    			<center>
+    		<h2 style="font-size: 20px;">Mhamad Othman Ibrahim</h2>
+    		<h3 style="font-size: 20px;">Student at IT Stage 3</h3>
+            <h3 style="font-size: 20px;">Age : 20</h3>
+            <h3 style="font-size: 20px;">Date of birth : 14/7/2001</h3>
+    			</center>
+    	</div>
+    	</div>
+
+    </div>
+</body>
+</html>
